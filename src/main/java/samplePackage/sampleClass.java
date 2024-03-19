93ec54f9bc8d0f4497be452be873883a3b8aa847
jongwoolee <jong168@naver.com>
2024-03-19 13:35:34 +0900

    .gitignore setting

---
+++ src/main/java/samplePackage/sampleClass.java
@@ -0,0 +1,7 @@
+package samplePackage;
+
+public class sampleClass{
+    public static void main(String[] args) {
+        System.out.println("this is sample class!");
+    }
+}
---
+++ src/test/java/samplePackage/sampleClassTest.java
@@ -0,0 +1,9 @@
+package samplePackage;
+
+public class sampleClassTest{
+    public static void main(String[] args) {
+        System.out.println("this is sample class test");
+    }
+
+
+}
