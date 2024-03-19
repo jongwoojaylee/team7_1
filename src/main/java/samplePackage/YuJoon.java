21a7a4305277d3913053302fd863f5f3a6ba4004
LeeYuJoon <leeyujoon97@gmail.com>
2024-03-19 16:39:03 +0900

    이유준

---
+++ src/main/java/samplePackage/YuJoon.java
@@ -0,0 +1,7 @@
+package samplePackage;
+
+public class YuJoon {
+    public static void main(String[] args) {
+        System.out.println("이유준");
+    }
+}
--- src/main/java/samplePackage/sampleClass.java
+++
@@ -1,7 +0,0 @@
-package samplePackage;
-
-public class sampleClass{
-    public static void main(String[] args) {
-        System.out.println("this is sample class!");
-    }
-}
