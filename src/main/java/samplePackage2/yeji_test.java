823e88852a68130c8d46f842d2e866a7498a1b79
yeji <dldpwl1705@naver.com>
2024-03-20 17:09:38 +0900

    leeyeji

--- src/main/java/samplePackage2/yeji_test.java
+++ src/main/java/samplePackage2/yeji_test.java
@@ -2,6 +2,6 @@ package samplePackage2;
 
 public class yeji_test {
     public static void main(String[] args) {
-        System.out.println("test leeyeji");
+        System.out.println("test leeyeji.");
     }
 }
